8e94c0a80bd1f2469fcc0fa71c0d7805674e76c3
System Administrator <leued2015@outlook.com>
2018-04-23 11:04:58 +0800

    add to git

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+const HelloWorld = () => import ('@/components/HelloWorld');
+const test = () => import ('@/pages/test/index');
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/test',
+      name: 'test',
+      component: test
+    }
+  ]
+})
